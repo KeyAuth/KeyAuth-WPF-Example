d57a27a7bef85416475d6b784316220d265f2892
It's Networking <87145183+Its-Networking@users.noreply.github.com>
2024-08-19 17:10:24 -0400

    Add files via upload

---
+++ KeyAuth WPF/Login.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using KeyAuth;
+
+namespace KeyAuth_WPF_Example
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        /// 
+        public static api KeyAuthApp = new api(
+            name: "", // Application Name
+            ownerid: "", // Owner ID
+            secret: "", // Application Secret
+            version: "" // Application Version /*
+           //path: @"Your_Path_Here" // (OPTIONAL) see tutorial here https://www.youtube.com/watch?v=I9rxt821gMk&t=1s
+        );
+
+        // VIEW https://keyauth.win/docs to see all of the functions that you can run.
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void Login_Loaded(object sender, RoutedEventArgs e)
+        {
+            await KeyAuthApp.init();
+            MessageBox.Show(KeyAuthApp.response.message + $"\n\n It took {api.responseTime}ms to Initialize.", "KeyAuth Response");
+        } 
+
+        private async void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            await KeyAuthApp.login(usernameField.Text, passwordField.Text);
+            if (KeyAuthApp.response.success)
+            {
+                Main main = new Main();
+                main.Show();
+                this.Close();
+
+                KeyAuthApp.log(usernameField.Text + " Logged In"); // this will send a log to the logs channel https://keyauth.win/app/?page=logs OR to your Discord server if you enabled Discord logs.
+            }
+            else
+            {
+                MessageBox.Show(KeyAuthApp.response.message, "KeyAuth Response");
+            }
+        }
+
+        private async void registerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string? email = emailField.Text;
+            if (!email.Contains("@"))
+            {
+                email = null;
+            }
+
+            await KeyAuthApp.register(usernameField.Text, passwordField.Text, email);
+            if (KeyAuthApp.response.success)
+            {
+                Main main = new Main();
+                main.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show(KeyAuthApp.response.message, "KeyAuth Response");
+            }
+        }
+
+        private async void upgradeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            await KeyAuthApp.upgrade(usernameField.Text, licenseField.Text);
+            MessageBox.Show(KeyAuthApp.response.message, "KeyAuth Response");
+        }
+
+        private async void licenseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            await KeyAuthApp.license(licenseField.Text);
+            if (KeyAuthApp.response.success)
+            {
+                Main main = new Main();
+                main.Show();
+                this.Close();
+
+                KeyAuthApp.log(licenseField.Text + " Logged In");
+            }
+            else
+            {
+                MessageBox.Show(KeyAuthApp.response.message, "KeyAuth Response");
+            }
+        }
+
+        private async void forgotPasswordBtn_Click(object sender, RoutedEventArgs e)
+        {
+            await KeyAuthApp.forgot(usernameField.Text, emailField.Text);
+            MessageBox.Show(KeyAuthApp.response.message);
+        }
+    }
+}
---
+++ KeyAuth WPF/Main.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KeyAuth_WPF_Example
+{
+    /// <summary>
+    /// Interaction logic for Main.xaml
+    /// </summary>
+    public partial class Main : Window
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private async void Main_Loaded(object sender, RoutedEventArgs e)
+        {
+            var onlineUsers = await Login.KeyAuthApp.fetchOnline();
+            if (onlineUsers != null)
+            {
+                foreach (var user in onlineUsers)
+                {
+                    onlineUsersField.Items.Add(user.credential + ", ");
+                }
+            }
+           
+            userDataField.Items.Add($"Username: {Login.KeyAuthApp.user_data.username}");
+            userDataField.Items.Add($"Expires: {UnixTimeToDateTime(long.Parse(Login.KeyAuthApp.user_data.subscriptions[0].expiry))}");
+            userDataField.Items.Add($"Subscription: {Login.KeyAuthApp.user_data.subscriptions[0].subscription}");
+            userDataField.Items.Add($"IP: {Login.KeyAuthApp.user_data.ip}");
+            userDataField.Items.Add($"HWID: {Login.KeyAuthApp.user_data.hwid}");
+            userDataField.Items.Add($"Creation Date: {UnixTimeToDateTime(long.Parse(Login.KeyAuthApp.user_data.createdate))}");
+            userDataField.Items.Add($"Last Login: {UnixTimeToDateTime(long.Parse(Login.KeyAuthApp.user_data.lastlogin))}");
+            userDataField.Items.Add($"Time Left: {expirydaysleft()}");
+        }
+
+        public string expirydaysleft()
+        {
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Local);
+            dtDateTime = dtDateTime.AddSeconds(long.Parse(Login.KeyAuthApp.user_data.subscriptions[0].expiry)).ToLocalTime();
+            TimeSpan difference = dtDateTime - DateTime.Now;
+            return Convert.ToString(difference.Days + " Days " + difference.Hours + " Hours Left");
+        }
+
+        public DateTime UnixTimeToDateTime(long unixtime)
+        {
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Local);
+            try
+            {
+                dtDateTime = dtDateTime.AddSeconds(unixtime).ToLocalTime();
+            }
+            catch
+            {
+                dtDateTime = DateTime.MaxValue;
+            }
+            return dtDateTime;
+        }
+    }
+}
